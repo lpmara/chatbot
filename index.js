7f895ed4f0ebfe0d6f403a4bde8140262f1268cb
Worawut <tonmanna@gmail.com>
2019-07-15 13:50:26 +0700

    commit cod to cloud repository

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("YOYO Hello word");
+});
+
+app.post("top-secret-bot", (req,res)=>{
+    const {space, type, message} = req.body || {};
+
+    if(type === "ADDED_TO_SPACE" && space.type === "ROOM"){
+        res.send({text: 'Thanks for adding me to ${space.displayName}'});
+    }
+});
+
+app.listen(PORT, ()=>{
+    console.log('listening on ${PORT}');
+});
